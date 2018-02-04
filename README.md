b33e4e6dc2b63d81ff44bf31129c93b3e82b442b
crispin <abraham.tecnokratix@gmail.com>
2018-02-03 18:15:04 -0600

    todo esta listo, clonalo

---
+++ README.md
@@ -0,0 +1,16 @@
+# Inventio Lite
+Inventio Lite es un sistema de Inventario y Ventas de proposito general desarrollado con PHP y MySQL.
+PROBADO PARA GIT
+
+## Modulos
+- Productos
+- Categorias
+- Caja
+- Clientes
+- Proveedores
+- Inventario
+- Usuarios
+
+## Mas informacion
+Encuentra mas informacion, instrucciones y demos en el siguiente link.
+Link: http://evilnapsis.com/2015/07/11/inventio-lite-sistema-de-inventario-y-ventas/
---
+++ core/app/view/home-view.php
@@ -0,0 +1,139 @@
+	<?php
+	$found=true;
+$products = ProductData::getAll();
+foreach($products as $product){
+	$q=OperationData::getQYesF($product->id);	
+	if($q<=$product->inventary_min){
+		$found=true;
+		break;
+
+	}
+}
+	?>
+<div class="row">
+	<div class="col-md-12">
+		<h1 style="text-align: center;">Bienvenido a Dulceria y Abarrotes | Rancho Nuevo</h1><br>
+</div>
+</div>
+  <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3><?php echo count(ProductData::getAll());?></h3>
+
+              <p>Productos</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="./?view=products" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-purple">
+            <div class="inner">
+              <h3><?php echo count(PersonData::getClients());?></h3>
+
+              <p>Clientes</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="./?view=clients" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3><?php echo count(PersonData::getProviders());?></h3>
+
+              <p>Proveedores</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="./?view=providers" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3><?php echo count(CategoryData::getAll());?></h3>
+
+              <p>Categorias</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="./?view=categories" class="small-box-footer">Ver mas <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+
+<div class="row">
+	<div class="col-md-12">
+<?php if($found):?>
+<div class="btn-group pull-right">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+    <i class="fa fa-download"></i> Descargar <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="report/alerts-word.php">Word 2007 (.docx)</a></li>
+  </ul>
+</div>
+<?php endif;?>
+
+</div>
+<div class="clearfix"></div>
+<?php if(count($products)>0){?>
+<br><table class="table table-bordered table-hover">
+	<thead>
+		<th >Codigo</th>
+		<th>Nombre del producto</th>
+		<th>En Stock</th>
+		<th></th>
+	</thead>
+	<?php
+foreach($products as $product):
+	$q=OperationData::getQYesF($product->id);
+	?>
+	<?php if($q<=$product->inventary_min):?>
+	<tr class="<?php if($q==0){ echo "danger"; }else if($q<=$product->inventary_min/2){ echo "danger"; } else if($q<=$product->inventary_min){ echo "warning"; } ?>">
+		<td><?php echo $product->id; ?></td>
+		<td><?php echo $product->name; ?></td>
+		<td><?php echo $q; ?></td>
+		<td>
+		<?php if($q==0){ echo "<span class='label label-danger'>No hay existencias.</span>";}else if($q<=$product->inventary_min/2){ echo "<span class='label label-danger'>Quedan muy pocas existencias.</span>";} else if($q<=$product->inventary_min){ echo "<span class='label label-warning'>Quedan pocas existencias.</span>";} ?>
+		</td>
+	</tr>
+<?php endif;?>
+<?php
+endforeach;
+?>
+</table>
+
+<div class="clearfix"></div>
+
+	<?php
+}else{
+	?>
+	<div class="jumbotron">
+		<h2>No hay alertas</h2>
+		<p>Por el momento no hay alertas de inventario, estas se muestran cuando el inventario ha alcanzado el nivel minimo.</p>
+	</div>
+	<?php
+}
+
+?>
+<br><br><br><br><br><br><br><br><br><br>
+	</div>
+</div>
